0d0b9edc1bed6ce8ddd51c98b0fdcee11a0f06b0
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-04-30 13:32:53 +0700

    profile

--- app/Services/UserPreferenceService.php
+++ app/Services/UserPreferenceService.php
@@ -2,7 +2,10 @@
 
 namespace App\Services;
 
+use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Support\Facades\Request;
 
 class UserPreferenceService
 {
@@ -12,10 +15,10 @@ class UserPreferenceService
     public function updatePreferences(User $user, array $preferences): User
     {
         $currentPreferences = $user->preferences ?? [];
-        
+
         // Merge new preferences with existing ones
         $updatedPreferences = array_merge($currentPreferences, $preferences);
-        
+
         $user->update([
             'preferences' => $updatedPreferences,
         ]);
@@ -29,5 +32,24 @@ class UserPreferenceService
     public function getUsersByPreference(string $key, mixed $value)
     {
         return User::where("preferences->$key", $value)->get();
+    }
+
+    public function updateProfile(int $user_id, ProfileUpdateRequest $request)
+    {
+        $profile = Profile::where('user_id', $request->user()->id)->first();
+
+        if (!$profile) {
+            $profile = new Profile();
+            $profile->create([
+                'user_id' => $request->user()->id,
+                'phone'   => $request->input('phone'),
+                'address' => $request->input('address')
+            ]);
+        } else {
+            $profile->update([
+                'phone'   => $request->input('phone'),
+                'address' => $request->input('address')
+            ]);
+        }
     }
 }
