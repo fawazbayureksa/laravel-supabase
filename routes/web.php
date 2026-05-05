0c3667ced34ff202e0a41f42d55287157de8e025
fawazbayureksa <fawwazbayureksa@gmail.com>
2026-05-05 13:12:05 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-});
+Route::get('/', [PostController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
